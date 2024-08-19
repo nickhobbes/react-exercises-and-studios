fe67e69a66e30da8eb7d9a2f40f2e1693924046a
Nicholas Cerame <nickcerame@Nicholass-MacBook-Air.local>
2024-08-19 08:56:08 -0500

    Part 1 of exercises completed. Book images and chores list added.

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,17 @@
+import classes from './BookList.module.css';
+
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book To Read";
+   let book1 = "https://m.media-amazon.com/images/I/71l6aEDzaFS._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/81g0AATkO9L._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/71lKy8RoFUL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} className={classes.bookImage} alt="The Adventures of Tom Sawyer" />
+         <img src={book2} className={classes.bookImage} alt="The Odyssey" />
+         <img src={book3} className={classes.bookImage} alt="The Lord of The Rings: The Fellowship of the Ring" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,13 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <div>
+         <p className={classes.choresHeading}>Chores TODO</p>
+         <ol>
+            <li className={classes.choresText}>Chore 1</li>
+            <li className={classes.choresText}>Chore 2</li>
+         </ol>
+      </div>
+   );
 }
