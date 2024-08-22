98288145c78052bcc09f342d989129be21f451b8
Nicholas Cerame <nickcerame@Nicholass-MacBook-Air.local>
2024-08-22 17:54:57 -0500

    Exercises part 2 completed

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -2,8 +2,12 @@ import './styles.css';
 
 function Button() {
 
+   const onLearnMore = () => {
+      alert("Splish Splash");
+   }
+
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
